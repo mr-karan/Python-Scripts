33e17354e7621fcc0b4f8e2adf762b81a4e59a31
Karan Sharma <karansharma1295@gmail.com>
2015-06-08 02:36:32 +0530

    Update remindme.py

--- remindme.py
+++ remindme.py
@@ -4,13 +4,13 @@ import time
 
 while True:
 	tomorrow = datetime.datetime.replace(datetime.datetime.now() + datetime.timedelta(days=1), hour=0, minute=0, second=0)
-									#Tomorrow Date , with time set as 00:00:00 
+	#Tomorrow Date , with time set as 00:00:00 
 
 	now = datetime.datetime.now()	#Present Time
 
 	counter = time.strftime("%d")  	#day of month
 
-	left = tomorrow - now			#Time left until next day 00:00:00
+	left = tomorrow - now		#Time left until next day 00:00:00
 
 	message = "Github Commit Streak.Day : %s \n Time Left for today %s :" % (counter, left)
 
